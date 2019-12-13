95fa569a94df5d3f650af0e63f769f0afd26de91
Gabe N <nelsong1997@gmail.com>
2019-12-13 15:33:37 -0500

    completed reset function

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ class App extends React.Component {
             green: 36,
             blue: 102,
             previousColors: [],
-            theInterval: null,
-            theTimeout: null
+            colorSurfInterval: null,
+            theTimeout: null,
+            hasBeenReset: false
         }
+        this.mouseUp = this.mouseUp.bind(this);
+        this.mouseDown = this.mouseDown.bind(this);
         this.changeColor = this.changeColor.bind(this);
     }
 
@@ -18,12 +21,31 @@ class App extends React.Component {
         this.surfColors()
     }
 
+    mouseDown() {
+        let theTimeout = setTimeout(() => {
+            this.setState({previousColors: [], hasBeenReset: true}) //reset
+            this.surfColors({
+                red: this.state.red,
+                green: this.state.green,
+                blue: this.state.blue
+            })
+        }, 2000)                                    //hold for 2 secs to reset
+        this.setState({theTimeout: theTimeout})
+    }
+
     mouseUp() {
+        this.setState({hasBeenReset: false})
         clearTimeout(this.state.theTimeout)
-        clearInterval(this.state.theInterval)
+        if (!this.state.hasBeenReset) {
+            clearInterval(this.state.colorSurfInterval)
+            this.changeColor()
+        }
+    }
+
+    changeColor() {
         let thePreviousColors = this.state.previousColors
         let theColors = [
-            {red: 44,  green: 44,  blue: 88 },     //default
+            {red: 44,  green: 44,  blue: 88 },     //default/indigo
             {red: 0,   green: 0,   blue: 0  },     //black
             {red: 73,  green: 9,   blue: 9  },     //red
             {red: 239, green: 90,  blue: 173},     //pink
@@ -58,23 +80,20 @@ class App extends React.Component {
         })
     }
 
-    mouseDown() {
-        let theTimeout = setTimeout(() => {
-            this.setState({previousColors: []})
-            this.surfColors()
-        }, 3000)
-        this.setState({theTimeout: theTimeout})
-    }
-
-    surfColors() {                          //setting up a color shifting wait screen
+    surfColors(colorInputObject) {                          //setting up a color shifting wait screen
         let redT = randomInteger(0, 100)   //initial values for the variable; sin of this variable will be the color value
         let greenT = randomInteger(0, 100)
         let blueT = randomInteger(0, 100)
+        if (colorInputObject) {                                //if from a reset; starting with the color we reset from
+            redT = Math.asin((colorInputObject.red - 128)/128)
+            greenT = Math.asin((colorInputObject.green - 128)/128)
+            blueT = Math.asin((colorInputObject.blue - 128)/128)
+        }
         let redRate = randomInteger(0, 100)/10000 + 0.005 //how fast this color value changes
         let greenRate = randomInteger(0, 100)/10000 + 0.005
         let blueRate = randomInteger(0, 100)/10000 + 0.005
 
-        let theInterval = setInterval(()=>{
+        let colorSurfInterval = setInterval(()=>{
             redT = redT + redRate
             greenT = greenT + greenRate
             blueT = blueT + blueRate
@@ -86,7 +105,7 @@ class App extends React.Component {
                 }
             )
         }, 17)
-        this.setState({theInterval: theInterval})
+        this.setState({colorSurfInterval: colorSurfInterval})
     }
 
     render() {
