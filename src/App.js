0952ea7d57a56a7f5d3d7ac17a7735d9c34eae47
Gabe N <nelsong1997@gmail.com>
2020-01-16 01:59:41 -0500

    increasing functionality of options menu

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends React.Component {
                 {red: 25,  green: 60,  blue: 9,   name: "green" },
                 {red: 49,  green: 115, blue: 198, name: "blue"  }
             ],
-            currentColor: 0
+            currentColor: 0,
+            pseudoRandom: true,
+            pseudoRandomCount: 2
         }
 
         this.mouseUp = this.mouseUp.bind(this);
@@ -31,6 +33,8 @@ class App extends React.Component {
         this.closeOptions = this.closeOptions.bind(this);
         this.deleteColor = this.deleteColor.bind(this);
         this.openDeleteDialog = this.openDeleteDialog.bind(this);
+        this.pseudoRandomCheckChange = this.pseudoRandomCheckChange.bind(this);
+        this.pseudoRandomCountChange = this.pseudoRandomCountChange.bind(this);
 
         this.deleteDialog = React.createRef();
     }
@@ -58,27 +62,36 @@ class App extends React.Component {
         }
     }
 
-    changeColor() {
+    changeColor() { //needs a way to confirm click registered, particularly when PR is disabled (count? text flash?)
         let thePreviousColors = this.state.previousColors
         let theColors = this.state.theColors
+        let pseudoRandom = this.state.pseudoRandom
+        let pseudoRandomCount = this.state.pseudoRandomCount
+
         let availableColors = [] //specifying which colors are viable to choose
-        for (let i = 0; i < theColors.length; i++) {
-            if (!thePreviousColors.includes(i)) {
-                availableColors.push(i)
+        if (pseudoRandom) {
+            for (let i = 0; i < theColors.length; i++) {
+                if (!thePreviousColors.includes(i)) {
+                    availableColors.push(i)
+                }
             }
+        } else {
+            availableColors = theColors //all colors are ok if PR is off
         }
 
         let chosenColorIndex = availableColors[randomInteger(0, availableColors.length - 1)] //choosing a new color to change to
 
-        thePreviousColors.push(chosenColorIndex) //updating previous colors array in state
-        if (thePreviousColors.length<3) {
-            this.setState({
-                previousColors: thePreviousColors
-            })
-        } else {
-            this.setState({
-                previousColors: thePreviousColors.slice(1,thePreviousColors.length)
-            })
+        if (pseudoRandom) { //we don't need to keep track of the prev colors if PR is off
+            thePreviousColors.push(chosenColorIndex) //updating previous colors array in state
+            if (thePreviousColors.length<=pseudoRandomCount) {
+                this.setState({
+                    previousColors: thePreviousColors
+                })
+            } else {
+                this.setState({
+                    previousColors: thePreviousColors.slice(1,thePreviousColors.length)
+                })
+            }
         }
 
         this.setState({ //changing the color
@@ -88,7 +101,7 @@ class App extends React.Component {
         })
     }
 
-    surfColors(colorInputObject) {                          //setting up a color shifting wait screen
+    surfColors(colorInputObject) {                    //setting up a color shifting wait screen; starts from color that is input, if there is one
         this.setState({surfingColors: true})
         let redT = randomInteger(0, 100)   //initial values for the variable; sin of this variable will be the color value
         let greenT = randomInteger(0, 100)
@@ -148,7 +161,7 @@ class App extends React.Component {
         let blueRate = Math.floor(initBlue/30) + 1
 
         let openOptionsInterval = setInterval(()=>{
-            if (this.state.red > 0 || this.state.green > 0 || this.state.blue > 0) {
+            if (this.state.red > 0 || this.state.green > 0 || this.state.blue > 0) { //this can result in negative values..reconsider?
                 this.setState({
                     red: this.state.red - redRate,
                     green: this.state.green - greenRate,
@@ -166,12 +179,26 @@ class App extends React.Component {
     }
 
     optionsMenu(optionsOpen, theColors) {
+        let pseudoRandom = this.state.pseudoRandom
         if (!optionsOpen) {
             return null
         } else {
             let colorArray = []
             let i = 0
             for (let color of theColors) {
+                let redX;
+                if (theColors.length===1) {
+                    redX = [
+                        <svg id={"red-x-" + i} className="red-x" viewBox="0 0 10 10"></svg> //empty transparent box where x would go--we can't delete our last color!
+                    ]
+                } else {
+                    redX = [
+                        <svg id={"red-x-" + i} className="red-x" viewBox="0 0 10 10" onClick={this.openDeleteDialog}>    {/* this is the x to remove a color*/}
+                            <polygon id={i} points="1,2 2,1 9,8 8,9" style={{fill: "red", stroke: "red", strokeWidth: "1"}}/>
+                            <polygon id={i} points="9,2 8,1 1,8 2,9" style={{fill: "red", stroke: "red", strokeWidth: "1"}} />
+                        </svg>
+                    ]
+                }
                 colorArray.push(
                     <div key={i} className="color-item">
                         <div className="color-item-inner">
@@ -187,18 +214,31 @@ class App extends React.Component {
                             </label>
                         </div>
                         <div className="color-item-inner">
-                            <svg className="edit-button" viewBox="0 0 10 10" onClick={()=>{}}>                    {/* this is the pencil icon for editing*/}
+                            <svg className="edit-button" viewBox="0 0 10 10" onClick={()=>{}}>            {/* this is the pencil icon for editing a color*/}
                                 <polygon points="1,9 2,7 8,1 9,2 3,8" style={{fill: "green", stroke: "green", strokeWidth: "1"}}/>
                             </svg>
-                            <svg id={"red-x-" + i} className="red-x" viewBox="0 0 10 10" onClick={this.openDeleteDialog}>                    {/* this is the x to remove a color*/}
-                                <polygon id={i} points="1,2 2,1 9,8 8,9" style={{fill: "red", stroke: "red", strokeWidth: "1"}}/>
-                                <polygon id={i} points="9,2 8,1 1,8 2,9" style={{fill: "red", stroke: "red", strokeWidth: "1"}} />
-                            </svg>
+                            {redX}
                         </div>
                     </div>
                 )
                 i++;
             }
+
+            let pseudoRandomCountSection; //we want to hide this section if PR is disabled
+            if (pseudoRandom) {
+                pseudoRandomCountSection = [
+                    <label className="indented"># of excluded colors: 
+                        <input 
+                            type="number" className="num-input" name="pseudo-random-count" //needs a check for posint in range
+                            min="1" max={this.state.theColors.length - 1}
+                            onChange={this.pseudoRandomCountChange} defaultValue="2"
+                        />
+                    </label>
+                ]
+            } else {
+                pseudoRandomCountSection = null;
+            }
+
             return ( 
                 <div id="options-menu">
                     <div id="the-x-div">
@@ -210,6 +250,12 @@ class App extends React.Component {
                     <h1>Options</h1>
                     <h2>Colors</h2>
                     {colorArray}
+                    <div className="options-section">
+                        <label>
+                            <input type="checkbox" name="pseudo-random-check" onChange={this.pseudoRandomCheckChange} defaultChecked/>pseudo randomness
+                        </label>
+                        {pseudoRandomCountSection}
+                    </div>
                 </div>
             )
         }
@@ -226,7 +272,6 @@ class App extends React.Component {
     }
 
     openDeleteDialog(event) {
-        console.log(Number(event.target.id.slice(-1)))
         this.setState( {currentColor: Number(event.target.id.slice(-1))} )
         this.deleteDialog.current.showModal();
     }
@@ -235,11 +280,30 @@ class App extends React.Component {
         let colorNumber = this.state.currentColor
         let theColors = this.state.theColors
         theColors.splice(colorNumber, 1)
-        this.setState( {theColors: theColors} )
+        this.setState( {theColors: theColors, currentColor: 0} )
+        //should check to see if you deleted too many colors & accordingly change PRC; plus if length is 1 PR change to false
+    }
+
+    pseudoRandomCheckChange(event) {
+        if (event.target.checked) {
+            this.setState( {pseudoRandom: true} )
+        } else {
+            this.setState( {pseudoRandom: false} )
+        }
+    }
+
+    pseudoRandomCountChange(event) {
+        this.setState( {pseudoRandomCount: Number(event.target.value)} )
     }
 
     render() {
         let color = this.state.theColors[this.state.currentColor]
+        let colorName = color.name
+        if (colorName==="") {             //not all colors have names ... let's not make this weird
+            colorName = "this color"
+        } else {
+            colorName = `"` + colorName + `"`
+        }
         return (
             <div
                 id="the-div"
@@ -250,7 +314,7 @@ class App extends React.Component {
                 {this.optionsMenu(this.state.optionsOpen, this.state.theColors)}
                 <dialog id="delete-dialog" ref={this.deleteDialog}>
                     <form method="dialog">
-                        <h4>Are you sure you want to delete "{color.name}"????</h4>
+                        <h4>Are you sure you want to delete {colorName}????</h4>
                         <p>r: {color.red} g: {color.green} b: {color.blue}</p>
                         <menu>
                             <button value="cancel">cancel</button>
