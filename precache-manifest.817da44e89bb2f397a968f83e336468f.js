465b46cf0f59c621269782790ab74bdc205e344d
Gabe N <nelsong1997@gmail.com>
2020-05-06 20:52:20 -0400

    Updates

---
+++ precache-manifest.817da44e89bb2f397a968f83e336468f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14954091fddf457a114aca60c6382e37",
+    "url": "/color_chooser/index.html"
+  },
+  {
+    "revision": "3ba94746bc3c2376a9a2",
+    "url": "/color_chooser/static/js/2.502d845d.chunk.js"
+  },
+  {
+    "revision": "68d645399571d40fd83a3bce9a0a3ebb",
+    "url": "/color_chooser/static/js/2.502d845d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236421c4a7e35a2c6c2e",
+    "url": "/color_chooser/static/js/main.d7d111b5.chunk.js"
+  },
+  {
+    "revision": "fcac6268329d77af1294",
+    "url": "/color_chooser/static/js/runtime-main.66d3027d.js"
+  }
+]);
